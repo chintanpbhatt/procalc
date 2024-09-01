0cefede0e97c9986e35a368ba2990b1876f2c341
chintan bhatt <chintan@gladminds.co>
2024-09-02 01:03:39 +0530

    N numbers with \n: a successful testcase

--- app/string_calculator.py
+++ app/string_calculator.py
@@ -1,12 +1,12 @@
+import re
+
+
 class StringCalculator:
     def add(self, numbers: str):
         if not numbers:
             return 0
 
-        if ',' in numbers:
-            numbers_str_list = numbers.split(',')
-            numbers_list = [float(string) for string in numbers_str_list]
-            return int(sum(numbers_list))
-
-        if len(numbers) >= 1:
-            return int(numbers)
+        delimiter = ',|\n'
+        numbers_str_list = re.split(delimiter, numbers)
+        numbers_list = [float(string) for string in numbers_str_list]
+        return int(sum(numbers_list))
