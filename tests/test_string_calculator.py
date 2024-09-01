90f6919a81920312d05530f134c34eab34bd6c5b
chintan bhatt <chintan@gladminds.co>
2024-09-02 00:31:51 +0530

    single number: a failed testcase

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -1,6 +1,19 @@
+import pytest
 from app.string_calculator import StringCalculator
 
 
 def test_add_empty_string():
     calculator = StringCalculator()
     assert calculator.add("") == 0
+
+
+@pytest.mark.parametrize(
+    "numbers, expected_sum",
+    [
+        ("5", 5),
+        ("10", 10),
+    ],
+)
+def test_add_single_number(numbers, expected_sum):
+    calculator = StringCalculator()
+    assert calculator.add(numbers) == expected_sum
