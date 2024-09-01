2ddb7d2a830920d317a273b3d51b6bf85c3204c2
chintan bhatt <chintan@gladminds.co>
2024-09-02 01:36:33 +0530

    N numbers with any delimiters: a successful testcase

--- app/string_calculator.py
+++ app/string_calculator.py
@@ -2,11 +2,19 @@ import re
 
 
 class StringCalculator:
+    DELIMITER_INDICATOR = '//'
+
     def add(self, numbers: str):
         if not numbers:
             return 0
 
         delimiter = ',|\n'
+
+        if numbers.startswith(StringCalculator.DELIMITER_INDICATOR):
+            parts = numbers.split('\n', maxsplit=1)
+            delimiter = parts[0][len(StringCalculator.DELIMITER_INDICATOR) :]
+            delimiter = re.escape(delimiter)
+            numbers = parts[1]
         numbers_str_list = re.split(delimiter, numbers)
         numbers_list = [float(string) for string in numbers_str_list]
         return int(sum(numbers_list))
