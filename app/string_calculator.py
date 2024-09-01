6de8cfeedbbad84a656aabaab88487bd16d7b984
chintan bhatt <chintan@gladminds.co>
2024-09-02 00:42:08 +0530

    two numbers: a successful testcase

--- app/string_calculator.py
+++ app/string_calculator.py
@@ -3,5 +3,10 @@ class StringCalculator:
         if not numbers:
             return 0
 
+        if ',' in numbers:
+            numbers_str_list = numbers.split(',')
+            numbers_list = [float(string) for string in numbers_str_list]
+            return int(sum(numbers_list))
+
         if len(numbers) >= 1:
             return int(numbers)
