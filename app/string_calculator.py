67f698b38a0919553edf513c2dac64ce3913a875
chintan bhatt <chintan@gladminds.co>
2024-09-04 17:42:30 +0530

    decorator added

--- app/string_calculator.py
+++ app/string_calculator.py
@@ -4,29 +4,39 @@ import re
 class StringCalculator:
     DELIMITER_INDICATOR = '//'
 
-    def add(self, numbers: str):
+    def parse_numbers(func):
+
+        def wrapper(*args, **kwargs):
+            numbers = args[1]
+            delimiter = ',|\n'
+            if numbers.startswith(StringCalculator.DELIMITER_INDICATOR):
+                parts = numbers.split('\n', maxsplit=1)
+                delimiter_section = parts[0][len(StringCalculator.DELIMITER_INDICATOR) :]
+                if delimiter_section.startswith('[') and delimiter_section.endswith(']'):
+                    # Handling multiple delimiters enclosed in square brackets
+                    delimiters = re.findall(r'\[(.*?)\]', delimiter_section)
+                    delimiter = '|'.join(re.escape(delim) for delim in delimiters)
+                else:
+                    delimiter = re.escape(delimiter_section)
+
+                numbers = parts[1]
+            numbers_str_list = re.split(delimiter, numbers)
+            m_args = list(args)
+            m_args[1] = numbers_str_list
+
+            return func(*m_args, **kwargs)
+
+        return wrapper
+
+    @parse_numbers
+    def add(self, numbers: list[str] | str):
         if not numbers:
             return 0
-
-        delimiter = ',|\n'
-
-        if numbers.startswith(StringCalculator.DELIMITER_INDICATOR):
-            parts = numbers.split('\n', maxsplit=1)
-            delimiter_section = parts[0][len(StringCalculator.DELIMITER_INDICATOR) :]
-            if delimiter_section.startswith('[') and delimiter_section.endswith(']'):
-                # Handling multiple delimiters enclosed in square brackets
-                delimiters = re.findall(r'\[(.*?)\]', delimiter_section)
-                delimiter = '|'.join(re.escape(delim) for delim in delimiters)
-            else:
-                delimiter = re.escape(delimiter_section)
-
-            numbers = parts[1]
-        numbers_str_list = re.split(delimiter, numbers)
 
         total = 0
         negatives = []
 
-        for num in numbers_str_list:
+        for num in numbers:
             if num:
                 n = float(num)
                 if n > 1000:
